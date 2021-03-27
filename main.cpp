9c6b3bf8b474a1600dd3b7c4f00f1b48cd873b91
Alexandru Puscasu <palexandru2000@gmail.com>
2021-03-27 15:16:23 +0200

    add more obj objects

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <ObjModel.h>
 
 int main() {
-    ObjModel obj("../objModels/cube_triangulated.obj");
+    //ObjModel obj("../objModels/cube_triangulated.obj"); // triangulated cube, small object
+    //ObjModel obj("../objModels/cube_untriangulated.obj"); // untriangulated cube, small object
+    ObjModel obj("../objModels/largeObject.obj"); // large object
     if (!obj.load())
         return -1;
 
